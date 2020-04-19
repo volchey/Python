442db62d946d463ee074092bf5a3afc1cc6865e7
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2020-04-19 18:03:50 +0300

    Parser added

---
+++ npuzzle.py
@@ -0,0 +1,6 @@
+#/usr/bin/python
+
+import parser
+
+if __name__ == "__main__":
+    puzzle = parser.parse_puzzles
---
+++ parser.py
@@ -0,0 +1,54 @@
+#/usr/bin/python
+
+import sys
+
+is_debug = True
+
+def is_comment(line):
+    return line[0] == '#'
+
+def get_puzzle_size(file):
+    for line in file:
+        if is_comment(line):
+            continue
+
+        size = int(line)
+        if (is_debug):
+            print('{} = {}'.format('puzzle size', size))
+        return size
+    return 0
+
+def parse_map(puzzle_size, file):
+    puzzle = []
+
+    for line in file:
+        row = map(int, line.split(' '))
+        if (len(row) != puzzle_size):
+            print('{}: {}'.format("incorrect puzzle map in row", line))
+            exit(2)
+
+        puzzle.append(row)
+
+    return puzzle
+
+def parse_puzzles():
+    if len(sys.argv) != 2:
+        print("parser.py [filename]")
+        exit(2)
+
+    file=open(sys.argv[1], "r")
+    
+    puzzle_size = get_puzzle_size(file)
+
+    if (puzzle_size <= 0):
+        print("incorrect puzzle size: ", puzzle_size)
+        exit(2)
+
+    puzzle = parse_map(puzzle_size, file)
+
+    if is_debug:
+        print("parsed puzzle:")
+        for row in puzzle:
+            print(row)
+    return puzzle
+    
