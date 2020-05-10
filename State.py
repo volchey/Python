9cd64d36d7e791704fffe6e4faff5cd8d0b1c1e9
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2020-05-10 12:52:56 +0300

    code style

--- State.py
+++ State.py
@@ -1,17 +1,17 @@
 #/usr/bin/python
 
-import math 
+import math
 from Functions import *
 from Puzzle import *
 
 class State():
     def __init__(self, func: Functions, puzzle: Puzzle, parent, is_debug = False):
-        self.func = func
-        self.puzzle = puzzle
-        self.parent = parent
-        self.is_debug = is_debug
-        self.steps_count = 0
-        self.heuristic_weight = 0
+        self.func               = func
+        self.puzzle             = puzzle
+        self.parent             = parent
+        self.is_debug           = is_debug
+        self.steps_count        = 0
+        self.heuristic_weight   = 0
 
     def __str__(self):
         return "Puzzle:\n{}Steps count (g) = {}\nHeuristic weight (h) = {}".format(
@@ -23,9 +23,6 @@ class State():
     def __lt__(self, other):
         return self.getWeight() < other.getWeight()
 
-    def __gt__(self, other):
-        return self.getWeight() > other.getWeight()
-
     def createNewStateFromIndex(self, index):
         new_puzzle = self.puzzle.getMovedToIndex(index)
         return State(self.func, new_puzzle, self, self.is_debug)
@@ -34,12 +31,12 @@ class State():
         index = self.puzzle.index
         curr_col, curr_row = self.func.getIndexCoords(index)
         states = []
-        
+
         left = index - 1
         left_col, left_row = self.func.getIndexCoords(left)
         if (left_row == curr_row and left_col >= 0):
             states.append(self.createNewStateFromIndex(left))
-        
+
         right = index + 1
         right_col, right_row = self.func.getIndexCoords(right)
         if (right_row == curr_row and right_col <= self.puzzle.size - 1):
@@ -49,12 +46,12 @@ class State():
         up_col, up_row = self.func.getIndexCoords(up)
         if (up_col == curr_col and up_row >= 0):
             states.append(self.createNewStateFromIndex(up))
-        
+
         down = index + self.puzzle.size
         down_col, down_row = self.func.getIndexCoords(down)
         if (down_col == curr_col and down_row <= self.puzzle.size - 1):
             states.append(self.createNewStateFromIndex(down))
-        
+
         return states
 
     def countHeuristicWeight(self):
@@ -89,7 +86,7 @@ class State():
 
     def getWeight(self):
         return self.steps_count + self.heuristic_weight
-    
+
     def isFinished(self):
         return self.func.goal_hash == self.puzzle.hash
 
