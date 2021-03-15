58dd1fa27d07e31fce208a795eb0b640e9d70e21
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2021-03-15 20:25:09 +0200

    Event_manage project created

---
+++ Event_manager/events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Event(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateTimeField(
+            default=timezone.now)
+
+    def __str__(self):
+        return self.title
