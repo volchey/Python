330d13dc1416957cff879bfd81a1d20529386773
Chechailiuk Volodymyr <chechailiuk@fabrica.net.ua>
2021-04-21 20:11:36 +0300

    date picker added + checkboxes for subjects

--- Event_manager/events/views.py
+++ Event_manager/events/views.py
@@ -18,16 +18,23 @@ def calendar(request):
     start_date = datetime.strptime(start_date_str, '%Y-%m-%d') if start_date_str else now
     end_date = datetime.strptime(end_date_str, '%Y-%m-%d') if end_date_str else now + timedelta(days=7)
 
+    all_subjects = Event.objects.filter(starttime__range=[start_date, end_date]).values_list('subject').distinct()
+
+    print(all_subjects)
+
     days = dict()
+    subjects = set()
     for curr_day in daterange(start_date, end_date):
         day_min = datetime.combine(curr_day, time.min)
         day_max = datetime.combine(curr_day, time.max)
-        print(day_min, day_max)
-        curr_events = Event.objects.filter(starttime__range=[day_min, day_max])
+        curr_events = Event.objects.filter(starttime__range=[day_min, day_max]).order_by('starttime')
 
         days[curr_day] = curr_events
 
-    return render(request, 'events/calendar.html', {'days': days})
+        for it_event in curr_events:
+            subjects.add(it_event.subject.description)
+
+    return render(request, 'events/calendar.html', {'days': days, 'start_date': start_date, 'end_date': end_date, 'subjects': subjects})
 
 class EventCreate(CreateView):
     model = Event
